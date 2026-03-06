c6aa83433d139682200cf338202c0071db6435de
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-06 17:52:26 +0530

    TestRestTemplate

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,23 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BeerControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testListBeers() {
+        BeerPagedList beerPagedList = restTemplate.getForObject("/api/v1/beer", BeerPagedList.class);
+
+        assertEquals(3, beerPagedList.getContent().size());
+    }
+}
